45ff5dd4471007b730452a3cc609462b35f3cc95
kimjihye058 <s2321@e-mirim.hs.kr>
2024-09-05 19:50:56 +0900

    이미지뷰와 이미지 버튼을 활용한 예제

--- andtest02/src/main/java/com/example/andtest02/MainActivity.kt
+++ andtest02/src/main/java/com/example/andtest02/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.andtest02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var btn3 = findViewById<TextView>(R.id.btn3)
+        setContentView(R.layout.layout_test3)
+        var edName = findViewById<EditText>(R.id.etName)
+        var btnOK = findViewById<Button>(R.id.btnOK)
 
-        btn3.setOnClickListener{
-            btn3.visibility= View.INVISIBLE
+        btnOK.setOnClickListener {
+            var name = edName.text
+            Toast.makeText(applicationContext, "입력한 이름 : " + name, Toast.LENGTH_SHORT).show()
         }
     }
 }
