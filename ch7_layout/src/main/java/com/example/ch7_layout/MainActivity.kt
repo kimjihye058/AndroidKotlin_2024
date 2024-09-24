70f58a1407e1321871e8217b3ea56398cb7b6357
kimjihye058 <s2321@e-mirim.hs.kr>
2024-09-24 19:57:25 +0900

    뷰바인딩

--- ch7_layout/src/main/java/com/example/ch7_layout/MainActivity.kt
+++ ch7_layout/src/main/java/com/example/ch7_layout/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.ch7_layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.ch7_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            binding.tv1.visibility = View.VISIBLE;
+            binding.tv2.visibility = View.INVISIBLE;
+            binding.tv3.visibility= View.INVISIBLE;
+        }
+        binding.btn2.setOnClickListener {
+            binding.tv2.visibility = View.VISIBLE;
+            binding.tv1.visibility = View.INVISIBLE;
+            binding.tv3.visibility= View.INVISIBLE;
+        }
+        binding.btn3.setOnClickListener {
+            binding.tv3.visibility = View.VISIBLE;
+            binding.tv1.visibility = View.INVISIBLE;
+            binding.tv2.visibility= View.INVISIBLE;
+        }
     }
 }
